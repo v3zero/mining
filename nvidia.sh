62d9479c2c6d3d62038a12194718144bfbb0eb7e
v3zero <35236895+v3zero@users.noreply.github.com>
2018-01-30 22:19:24 +0000

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ethminer -U -F http://eth-eu.dwarfpool.com:80/59e80b863b0f44b0d7de82fe4004481195bf5767/onda_2/moja.kopalnia@gmail.com --cuda-block-size 256 --cuda-grid-size 16384 --cuda-streams 6 --cuda-schedule auto --cuda-parallel-hash 8 --dag-load-mode parallel --api-port 0 -v 1
+ethminer -U -F http://eth-eu.dwarfpool.com:80/0x5321b10ae52c32c6c65dc0e2cfd7229c6ada0d43/onda_2/moja.kopalnia@gmail.com --cuda-block-size 256 --cuda-grid-size 16384 --cuda-streams 6 --cuda-schedule auto --cuda-parallel-hash 8 --dag-load-mode parallel --api-port 0 -v 1
